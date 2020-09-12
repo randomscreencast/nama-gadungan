eecc2e8a26925a1d4be5d81e95eecade5eadca02
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-09-12 23:50:23 +0700

    add test more than two character names

--- src/all.test.js
+++ src/all.test.js
@@ -1,8 +1,15 @@
-const all = require('./all.js');
+const all = require("./all.js");
 
-test('it works!', function() {
-  const actual = all().every(function(currentValue) {
-    return typeof currentValue === 'string';
+test("it works!", function () {
+  const actual = all().every(function (currentValue) {
+    return typeof currentValue === "string";
+  });
+  expect(actual).toBe(true);
+});
+
+test("Jumlah karakter nama lebih dari dua", function () {
+  const actual = all().every(function (currentValue) {
+    return currentValue.length > 2;
   });
   expect(actual).toBe(true);
 });
