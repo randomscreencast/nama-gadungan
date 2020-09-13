a14de3590be20567b4eb11725903cd5a64813399
Riza Fahmi <rizafahmi@gmail.com>
2020-09-13 19:59:06 +0700

    Add eslint jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-      "ecmaVersion": 12
-    },
-    "rules": {
-      "semi": ["error", "always"],
-      "indent": ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      "quotes": ["error", "single"]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: ["jest"],
+  rules: {
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"]
+  }
 };
