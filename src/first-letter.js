354e04a8653aad0c8f6de8d6903a9fca71eae697
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-09-15 22:12:40 +0700

    add getNameByFirstLate feature

---
+++ src/first-letter.js
@@ -0,0 +1,10 @@
+const names = require("./data/names.json");
+
+function getNameByfirstLetter(firstLetter){
+    if(firstLetter.length !== 1 ) return [];
+
+    const filteredNameByFirstLetter = names.filter(name => name.charAt(0).toLowerCase() === firstLetter.toLowerCase());
+    return filteredNameByFirstLetter;
+}
+
+module.exports = getNameByfirstLetter;
