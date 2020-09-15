9c44597e3d7dee482fbec20edd81bef67d15e26d
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-09-15 22:17:24 +0700

    make script in first-letter.js messy

--- src/first-letter.js
+++ src/first-letter.js
@@ -1,10 +1,12 @@
 const names = require("./data/names.json");
 
-function getNameByfirstLetter(firstLetter){
-    if(firstLetter.length !== 1 ) return [];
+function getNameByfirstLetter(firstLetter) {
+  if (firstLetter.length !== 1) return [];
 
-    const filteredNameByFirstLetter = names.filter(name => name.charAt(0).toLowerCase() === firstLetter.toLowerCase());
-    return filteredNameByFirstLetter;
+  const filteredNameByFirstLetter = names.filter(
+    (name) => name.charAt(0).toLowerCase() === firstLetter.toLowerCase()
+  );
+  return filteredNameByFirstLetter;
 }
 
 module.exports = getNameByfirstLetter;
