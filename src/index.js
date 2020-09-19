b940e4ea94494154d0cad4c0a87fa40cefe706b4
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-09-19 21:48:28 +0700

    add function randomAddress and test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const all = require("./all.js");
 const random = require("./random.js");
 const allAddress = require("./allAddress");
+const randomAddress = require("./randomAddress");
 
-module.exports = {all, random, allAddress};
+module.exports = {all, random, allAddress, randomAddress};
