933a5ea42a95a59a6ee8b9576cf80f0d7ba08f66
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-09-19 16:22:51 +0700

    add function allAddress and randomAddress to make fake address anda i make too file test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const all = require("./all.js");
 const random = require("./random.js");
 const allAddress = require("./allAddress.js");
+const randomAddress = require("./randomAddress.js");
 
 module.exports = {
   all,
   allAddress,
-  random
+  random,
+  randomAddress
 };
