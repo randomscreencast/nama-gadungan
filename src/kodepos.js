9e90c685d4d227b2ae3f554010c1856623f2856f
Riza Fahmi <rizafahmi@gmail.com>
2020-09-27 20:28:40 +0700

    Add kodepos tambun selatan

--- src/kodepos.js
+++ src/kodepos.js
@@ -1,6 +1,8 @@
 function getKodepos(location) {
   if (location === "Tangerang") {
     return 15214;
+  } else if(location === "Tambun Selatan") {
+    return 1750;
   } else {
     return 12345;
   }
