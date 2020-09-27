1d3860313b5d63691e1872d58c7a929a613273f4
Riza Fahmi <rizafahmi@gmail.com>
2020-09-27 20:41:43 +0700

    Add new kodepos and add more test case

--- src/kodepos.js
+++ src/kodepos.js
@@ -1,8 +1,10 @@
 function getKodepos(location) {
   if (location === "Tangerang") {
     return 15214;
-  } else if(location === "Tambun Selatan") {
-    return 1750;
+  } else if (location === "Tambun Selatan") {
+    return 17510;
+  } else if (location === "Manggala") {
+    return 90233;
   } else {
     return 12345;
   }
--- src/kodepos.test.js
+++ src/kodepos.test.js
@@ -4,3 +4,18 @@ test("getKodepos for Tangerang", function () {
   const actual = getKodepos("Tangerang");
   expect(actual).not.toBeNaN();
 });
+
+test("getKodepos for Tambun Selatan", function () {
+  const actual = getKodepos("Tambun Selatan");
+  expect(actual).not.toBeNaN();
+});
+
+test("getKodepos for Manggala", function () {
+  const actual = getKodepos("Manggala");
+  expect(actual).not.toBeNaN();
+});
+
+test("getKodepos for else", function () {
+  const actual = getKodepos("Bekasi Timur");
+  expect(actual).not.toBeNaN();
+});
